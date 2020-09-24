25fd50147eb50f8e8cac5938d153e45125a67ef2
Karina Guerra <kpguerra96@gmail.com>
2020-09-24 13:33:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -198,8 +198,8 @@ end
 def big_shoe_rebounds
   shoe_array = []
   players = game_hash[:home][:players].concat(game_hash[:away][:players])
-  players.max_by do {|player_stats| }
-    player_stats[:shoe]
+  players.max_by do {|player_stats| player_stats[:shoe]}
+    player_stats[:rebounds]
     end
   binding.pry
 end
