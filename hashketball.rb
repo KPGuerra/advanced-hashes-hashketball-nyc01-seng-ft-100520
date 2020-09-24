aa359d0c71fd305043b54e9f72ed0a4e21ac189e
Karina Guerra <kpguerra96@gmail.com>
2020-09-24 13:08:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,7 +196,7 @@ def player_stats(name)
 end 
 
 def big_shoe_rebounds
-  game_hash.find do |location, teams|
+  game_hash.each do |location, teams|
     teams[:players].each do |data|
       if data[:shoe].max
         binding.pry
